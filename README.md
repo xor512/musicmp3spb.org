a6743bebf0a45906772445c0ba610a8b02536de7
RIAGUZOW, SIERGIEJ <siergiej.riaguzow@redembedded.com>
2017-06-11 02:00:42 +0200

    Copied from https://launchpad.net/musicmp3spb.org and added README.md

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # musicmp3spb.org
+
 Python script to automatically download albums from http://musicmp3spb.org site
+
+## Usage
+
+#### Download all songs from the album's page:
+
+```sh
+$ musicmp3spb.py http://musicmp3spb.org/album/dualism.html
+```
+
+#### Download all albums from the band's page:
+
+```sh
+$ musicmp3spb.py -a http://musicmp3spb.org/artist/textures.html
+
+```
+
+## Examples
+
+#### Download all albums from http://musicmp3spb.org/artist/intronaut.html:
+
+```sh
+[user@linux Intronaut]$ musicmp3spb.py -a http://musicmp3spb.org/artist/intronaut.html
+-------------------------------------------------------------------------------
+  Album "Habitual Levitations"
+  http://musicmp3spb.org/album/habitual_levitations.html
+-------------------------------------------------------------------------------
+01-killing_birds_with_stones.mp3                11.05 of 11.05 MB [100%]
+02-the_welding.mp3                              08.26 of 08.26 MB [100%]
+03-steps.mp3                                    07.87 of 07.87 MB [100%]
+04-sore_sight_for_eyes.mp3                      07.58 of 07.58 MB [100%]
+05-milk_leg.mp3                                 09.31 of 09.31 MB [100%]
+06-harmonomicon.mp3                             08.96 of 08.96 MB [100%]
+07-eventual.mp3                                 09.27 of 09.27 MB [100%]
+08-blood_from_a_stone.mp3                       04.25 of 04.25 MB [100%]
+09-the_way_down.mp3                             12.32 of 12.32 MB [100%]
+-------------------------------------------------------------------------------
+  Album "Void"
+  http://musicmp3spb.org/album/void_intronaut.html
+-------------------------------------------------------------------------------
+01-a_monumental_vulgarity.mp3                   13.71 of 13.71 MB [100%]
+02-gleamer.mp3                                  12.45 of 12.45 MB [100%]
+03-fault_lines.mp3                              14.69 of 14.69 MB [100%]
+04-nostalgic_echo.mp3                           12.16 of 12.16 MB [100%]
+05-teledildonics.mp3                            16.36 of 16.36 MB [100%]
+06-iceblocks.mp3                                14.98 of 14.98 MB [100%]
+07-rise_to_the_midden.mp3                       14.90 of 14.90 MB [100%]
+[user@linux Intronaut]$
+```
+
+## Requirements
+
+You need Python2 (since mechanize is not available for Pyton3) and [mechanize](https://pypi.python.org/pypi/mechanize/).
+
+## License
+
+[WTFPL – Do What the Fuck You Want to Public License](http://www.wtfpl.net/)
---
+++ musicmp3spb.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2017 Siergiej Riaguzow <xor256@gmx.com>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+import mechanize
+import re
+import urllib2
+import sys
+import os
+import shutil
+from urlparse import urlparse
+
+MAX_DOWNLOAD_ATTEMPTS = 5
+
+failed_album_urls = []
+song_found = False
+
+def print_error(msg):
+    color_red_bold_esc_seq = "\033[1;31m"
+    color_clear_esc_seq = "\033[0;39m"
+    print >> sys.stderr, '{}{}{}'.format(
+        color_red_bold_esc_seq,
+        msg,
+        color_clear_esc_seq)
+
+def to_MB(a_bytes):
+    return a_bytes / 1024. / 1024.
+
+def to_utf8(s):
+    """ musicmp3spb.org site uses CP1251 encoding however different versions
+        of python-mechanize for Python 2.7 behave differently: older versions
+        return str objects in CP1251 charset and newer ones detect encoding
+        automatically and return unicode objects - convert them all to UTS-8
+        to enable string comparisons with UTF-8 literals in thus source file
+    """
+    if isinstance(s, unicode):
+        return s.encode('utf8')
+    elif isinstance(s, str):
+        return s.decode('cp1251').encode('utf8')
+
+def download_file(url, filename):
+    u = urllib2.urlopen(url)
+    f = open(filename, 'wb')
+    size = int(u.info().getheaders('Content-Length')[0])
+
+    dlded = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        dlded += len(buffer)
+        f.write(buffer)
+
+        status = r'%-40s        %05.2f of %05.2f MB [%3d%%]' % \
+            (to_utf8(filename), to_MB(dlded), to_MB(size), dlded * 100. / size)
+        sys.stdout.write(status)
+        sys.stdout.flush()
+        backspaces = chr(8)*(len(status))
+        sys.stdout.write(backspaces)
+
+    sys.stdout.write('\n')
+    f.close()
+
+def browser_open(url):
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    browser.open(url)
+    return browser
+
+def to_absolute_url(url, browser):
+    if not url.startswith('http://') and not url.startswith('https://'):
+        hostname = urlparse(browser.geturl()).hostname
+        return 'http://' + hostname + url
+    return url
+
+def prepend_http(url):
+    if not url.startswith('http://') and not url.startswith('https://'):
+        return 'http://' + url
+    return url
+
+def mkdir_and_chdir(new_dir):
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+    os.chdir(new_dir)
+
+def download_song(url, filename_ref):
+    browser = browser_open(url)
+
+    action_regex = re.compile('/file/.*')
+
+    filename = url + '.mp3'
+    for link in browser.links(url_regex=action_regex):
+        filename = link.text
+        break
+
+    filename_ref['val'] = filename
+
+    formcount=0
+    for form in browser.forms():
+        if action_regex.match(form.attrs['action']):
+            break
+        formcount=formcount+1
+
+    browser.select_form(nr=formcount)
+    browser.submit()
+
+    for link in browser.links(url_regex='tempfile\.ru'):
+        global song_found
+        song_found = True
+        download_file(to_utf8(link.url), filename)
+        break
+
+def download_album(url):
+    browser = browser_open(url)
+
+    title_regex = re.compile('.*Скачать mp3.*', re.IGNORECASE)
+
+    for link in browser.links(url_regex='/download/.*'):
+        for attr in link.attrs:
+            if len(attr) == 2 and attr[0] == 'title':
+                # title looks like: Скачать mp3 Arms Of The Sea,
+                # links with other titles can be something like /download/play...
+                # and this is not what we want to download
+                if title_regex.match(to_utf8(attr[1])):
+                    attempts = MAX_DOWNLOAD_ATTEMPTS
+                    while attempts > 0:
+                        filename_ref = { 'val': str() }
+                        try:
+                            song_url = to_absolute_url(to_utf8(link.url), browser)
+                            download_song(song_url, filename_ref)
+                            break
+                        except UnicodeDecodeError:
+                            filename = filename_ref['val']
+                            if filename and os.path.exists(filename):
+                                os.remove(filename)
+                            raise e
+                        except Exception as e:
+                            attempts -= 1
+                            print_error('\nFailure, attempts left: %d, song: %s\n\t%s!' % \
+                                (attempts, song_url, e))
+                            filename = filename_ref['val']
+                            if filename and os.path.exists(filename):
+                                os.remove(filename)
+                            # Rethrow exception to the upper level to stop
+                            # downloading everything in case we cannot download
+                            # one song 5 times
+                            if attempts == 0:
+                                raise e
+
+def download_band(url):
+    browser = browser_open(url)
+
+    first_link = True
+    for link in browser.links(url_regex='/album/.*'):
+        album_url = to_absolute_url(to_utf8(link.url), browser)
+        album_name = to_utf8(link.text)
+        if not album_name: # Invisible, there will be another one with text
+            continue
+        album_dir = './' + album_name
+
+        print '-------------------------------------------------------------------------------'
+        print '  Album "%s"' % album_name
+        print '  %s' % album_url
+        print '-------------------------------------------------------------------------------'
+
+        attempts = MAX_DOWNLOAD_ATTEMPTS
+        global failed_album_urls
+        while attempts > 0:
+            mkdir_and_chdir(album_dir)
+            try:
+                download_album(album_url)
+                os.chdir('..')
+                break
+            except UnicodeDecodeError:
+                print_error("""\nDue to bug in httplib (http://bugs.python.org/issue11898)
+and mechanize not being available for Python3 this album cannot
+be downloaded automatically. You have to download this album
+by other means. (See list of such album links in the end).
+""")
+                failed_album_urls.append(album_url)
+                os.chdir('..')
+                shutil.rmtree(album_dir, ignore_errors=True)
+                break
+            except Exception as e:
+                attempts -= 1
+                print_error('\nFailure, attempts left: %d, album: %s\n\t%s!' % \
+                    (attempts, album_url, e))
+                failed_album_urls.append(album_url)
+                os.chdir('..')
+                shutil.rmtree(album_dir, ignore_errors=True)
+
+    if failed_album_urls:
+        print_error('\nFailed to download the following albums:')
+        for album_url in failed_album_urls:
+            print_error('\t' + album_url)
+
+def help():
+    print """Python script to automatically download albums from http://musicmp3spb.org site.
+
+Usage:
+
+$ musicmp3spb.py http://musicmp3spb.org/album/dualism.html
+to download all songs from the album's page
+
+or
+$ musicmp3spb.py -a[--all] http://musicmp3spb.org/artist/textures.html
+to download all albums from the band's page.
+
+You need Python2 (since mechanize is not available for Pyton3)
+and mechanize (https://pypi.python.org/pypi/mechanize)."""
+
+def main():
+    if len(sys.argv) < 2 or \
+       len(sys.argv) > 2 and \
+       sys.argv[1] != '-a' and sys.argv[1] != '--all':
+        script_name = os.path.basename(sys.argv[0])
+        print_error('Usage: %s <album_url> or %s -a[--all] <band_url>' % (script_name, script_name))
+        print_error('To print help: %s -h[--help]' % script_name)
+        sys.exit(1)
+
+    arg = sys.argv[1]
+    was_error = False
+
+    if arg == '-h' or arg == '--help':
+        help()
+    elif arg == '-a' or arg == '--all':
+        url = prepend_http(sys.argv[2])
+        try:
+            download_band(url)
+        except UnicodeDecodeError:
+            was_error = True
+            print_error("""\nDue to bug in httplib (http://bugs.python.org/issue11898)
+and mechanize not being available for Python3 these albums cannot
+be downloaded automatically. You have to download these albums
+by other means.
+""")
+        except Exception as e:
+            was_error = True
+            print_error('Error: Cannot download band albums: %s\n\t%s!' % (url, e))
+    else:
+        url = prepend_http(sys.argv[1])
+        try:
+            download_album(url)
+        except UnicodeDecodeError:
+            was_error = True
+            print_error("""\nDue to bug in httplib (http://bugs.python.org/issue11898)
+and mechanize not being available for Python3 this album cannot
+be downloaded automatically. You have to download this album
+by other means.
+""")
+        except Exception as e:
+            was_error = True
+            print_error('Error: Cannot download album: %s\n\t%s!' % (url, e))
+
+        if not was_error and not song_found:
+            print_error("Found nothing to download!")
+
+if __name__ == '__main__':
+    main()
